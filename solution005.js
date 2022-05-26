0586a9bba6c61866d1a6f6a8112948ccf25fb104
Shawn Gregory <shawn.gregory416@outlook.com>
2022-05-25 20:00:16 -0700

    solution 005 completed, return strings with defined variables completed

---
+++ solution005.js
@@ -0,0 +1,7 @@
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+//Shawn's notes this is simple to achieve, just intake a name variable input and return the welcome message with the definition using name definition
+name = "shawn" //create the name variable, define for the test.
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;//return the welcome mesage with the definition of name. using ${} and backticking `
+  }
